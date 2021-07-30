a0114301ecce16ff0a6f75f7f1436e1051e7b808
Alex Semochkin <fennyflop@gmail.com>
2021-07-31 00:16:16 +0300

    finished party info popup

--- src/components/PartyInfoPopup/PartyInfoPopup.js
+++ src/components/PartyInfoPopup/PartyInfoPopup.js
@@ -6,19 +6,25 @@ const PartyInfoPopup = () => {
     const placeIcon = `https://static.thenounproject.com/png/396728-200.png`;
 
     return (
-        <section className="info">
+        <section className="info info__shown">
             <div className="info__details">
-                <div>
+                <div className="info__part">
                     <img className="info__icon" alt="Party name" src={labelIcon} />
                     <p className="info__text">Re:Flex party</p>
                 </div>
                 <p className="info__text">15$ Вход</p>
-                <div>
+                <div className="info__part">
                     <img className="info__icon" alt="Party name" src={placeIcon} />
                     <p className="info__text">Bedford st. 123</p>
                 </div>
                 <p className="info__text">22:00</p>
             </div>
+            <button className="info__button info__details-button">
+                Подробности
+            </button>
+            <button className="info__button">
+                <span className="info__colorful">Записаться</span>
+            </button>
         </section>
     );
 }
